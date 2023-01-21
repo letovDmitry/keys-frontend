5c7583280ff25b5916c96a55f9de2c5346a5af93
Dmitry Letov <67585149+letovDmitry@users.noreply.github.com>
2023-01-21 20:53:44 +0600

    refactor

--- src/api/api.js
+++ src/api/api.js
@@ -1,7 +1,7 @@
 import axios from "axios"
 // const https = require('https')
 
-const baseURL = 'https://keys-store.online:8080/api'
+const baseURL = 'https://keys-store.online/api'
 
 const token = localStorage.getItem('jwt')
 
